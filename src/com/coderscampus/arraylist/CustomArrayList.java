0dcac2a2134f6ba27a42e8dd94b35ce30450b90d
Abagels96 <abmen18@gmail.com>
2024-10-13 22:38:19 -0500

    more work

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -3,7 +3,6 @@ package com.coderscampus.arraylist;
 
 import java.util.Arrays;
 
-
 // to add an element to an array we need to 
 //make sure that it is still the same array 
 //a. check and see if there is an index that we can add to 
@@ -13,87 +12,70 @@ import java.util.Arrays;
 
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
-	int size=0;
-	
-	
+	int size = 0;
 
 	@Override
 	public boolean add(T item) {
-			System.out.println(Arrays.toString(items));
-			if(size==items.length) {
-				System.out.println("Array is full");
-				arrayIsFull(items);}
-			//if array is full then 
-			
-			else	if(items[size]==null) {
-				
-//	items[i]=item;
-	items[size]=item;// adding each item
-	size++;
-				System.out.println(item);
-
-				return true;
-			}
-				
-			System.out.println(size);
-			
 		
-			
-	return false;
-			
-			
-	
+		System.out.println(Arrays.toString(items));
+		
+		
+		if (size == items.length) {
+			System.out.println("Array is full");
+			arrayIsFull(items);
+		}
+		// if array is full then
+
+		else if (items[size] == null) {
+
+			items[size] = item;// adding each item
+			System.out.println(item);
+
+			return true;
+		}
+		size++;
+
+		System.out.println(size);
+
+
+		return false;
+
 	}
 
+	public Object[] arrayIsFull(Object[] items) {
 
+		items = Arrays.copyOf(items, items.length * 2);
 
-
-
-
-
-	public Object[]  arrayIsFull( Object [] items
-			) {
-
-		
-			
-		items=Arrays.copyOf(items, items.length*2);
-		
-		
-				
 		System.out.println(Arrays.toString(items));
-			
-		return items;
-	
-	}{
-}
 
-		
-			
-			
-			
+		return items;
+
+	}
+
+	{
+	}
 
 	@Override
 	public int getSize() {
-		//put different method in case Object are added to the list 
-		
-		if(items!=null) {
+		// put different method in case Object are added to the list
+
+		if (items != null) {
 			System.out.println(items.length);
-		return items.length;
+			return items.length;
 		}
-		
-	
+
 		return 0;
 	}
 
 	@Override
 	public T get(int index) {
 
-			if (items[index]!=null) {
-				System.out.println(items[index]);
-				
+		if (items[index] != null) {
+			System.out.println(items[index]);
+
 		}
-		
+
 		return null;
 	}
-	
+
 }
