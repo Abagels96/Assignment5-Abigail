226e02589e882d30fb7b166f5049e93da7510b47
Abagels96 <abmen18@gmail.com>
2024-10-17 20:52:16 -0500

    something

--- src/com/coderscampus/arraylist/CustomArrayList.java
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -12,97 +12,75 @@ import java.util.Arrays;
 
 public class CustomArrayList<T> implements CustomList<T> {
 	Object[] items = new Object[10];
-	// 
+	//
 	int size = 0;
 
 	@Override
 	public boolean add(T item) {
-			
+
 		//
+
+		if (size == items.length) {
+			System.out.println("Array is full");
+			items = arrayIsFull(items);
+
+		}
+		else {
+			items[size] = item;
+
+			size++;
+			return true;}
 		
-			if (size == items.length) {
-				System.out.println("Array is full");
-				items=arrayIsFull(items);
-				
-				System.out.println(Arrays.toString(items));
-			}
-			
-			
-			for(int i=0; i<items.length;i++) {
-				items[size]=item;
-				
-				System.out.println(item);
-				size++;
-				System.out.println(size);
-				System.out.println(Arrays.toString(items));
-				return true;
-			}
-		
+
 		// if array is full then double the items
-
-	
-
-
-		
-
-
 
 		return false;
 
 	}
 
-	public Object[] arrayIsFull(Object[] items) {
-
-		items = Arrays.copyOf(items, items.length * 2);
-
-		System.out.println(Arrays.toString(items));
-
-
-		return items;
-	}
 
 	{
 	}
 
-//	I reviewed your get and getSize methods. The get method should return the item at the specified index, not null, and it should check if the index is within the bounds of the array.
-//			The getSize method should return the number of items in the list, not the length of the underlying array. It should return size, not items.length.
 	@Override
 	public int getSize() {
-int counter=0;
+		int counter = 0;
 
-for(Object item:items) {
-//counter updates when there is an item in that spot and when counter is less than items.length else it is supposed to print out the counter in the console and then return it
-		if (item != null) {
-			
-			 counter++;
-		} 
-		else   {		
-		
-			 System.out.println(counter);
-			 return counter;
-}
-		
-		
-}
-System.out.println(counter);
+		for (Object item : items) {
+			if (item != null) {
+
+				counter++;
+			} else {
+
+				System.out.println(counter);
+				return counter;
+			}
+
+		}
+		System.out.println(counter);
 
 		return 0;
 	}
 
 	@Override
 	public T get(int index) {
-		
-		
-		int i=index;
-		if(index>=0 && index<items.length){
-// if index is at least zero and less than the length of the array
-		// link to newItems or items pick one or the other
-		if (items[index] != null) {
-			System.out.println(items[index]);
-             return (T) items[i];
-		}}
-		
+
+		int i = index;
+		if (index >= 0 && index < items.length) {
+			if (items[index] != null) {
+				System.out.println(items[index]);
+				return (T) items[i];
+			}
+		}
+
 		return null;
+	}
+	public Object[] arrayIsFull(Object[] items) {
+		
+		items = Arrays.copyOf(items, items.length * 2);
+		
+		
+		return items;
 	}
 
 }
